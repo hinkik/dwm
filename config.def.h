5b6c7e8d1a8f39a9dfc37e4655f010454c4fc6d8
Henrik Hansson <hinkik@gmail.com>
2021-06-27 14:38:29 +0200

    change icon

--- config.def.h
+++ config.def.h
@@ -66,7 +66,7 @@ static const int resizehints = 1;    /* 1 means respect size hints in tiled resi
 static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ " ",      tile },    /* first entry is default */
-	{ " ",      NULL },    /* no layout function means floating behavior */
+	{ " ",      NULL },    /* no layout function means floating behavior */
 	{ " ",      monocle },
 };
 
