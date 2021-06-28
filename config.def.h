6012688511f804acc6c823bd2b9ebb45e66f7d94
Henrik Hansson <hinkik@gmail.com>
2021-06-28 13:11:55 +0200

    right padding

--- config.def.h
+++ config.def.h
@@ -11,8 +11,8 @@ static const int smartgaps          = 0;        /* 1 means no outer gap when the
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const int barhpadding        = 2;        /* 0 means bottom bar */
-static const int barvpadding        = 8;        /* 0 means bottom bar */
+static const int barhpadding        = 20;        /* 0 means bottom bar */
+static const int barvpadding        = 4;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=11" };
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
