e944815a3aa617d3780de52e65028cd53a6df234
Henrik Hansson <hinkik@gmail.com>
2021-07-02 00:30:25 +0200

    add plainotes scratchpads

--- config.def.h
+++ config.def.h
@@ -3,20 +3,20 @@
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 4;       /* snap pixel */
-static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappih    = 14;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 14;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 14;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 14;       /* vert outer gap between windows and screen edge */
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int barhpadding        = 20;
-static const int barvpadding        = 4;
-static const unsigned int baralpha = 170;
+static const int barvpadding        = 6;
+static const unsigned int baralpha = 204;
 static const unsigned int borderalpha = OPAQUE;
-static char font[]            = "monospace:size=11";
-static const char *fonts[]          = { font };
+static char font[]            = "monospace:size=10";
+static const char *fonts[]          = { font, "Noto Color Emoji:size=10" };
 static char normbgcolor[]           = "#222222";
 static char normbordercolor[]       = "#444444";
 static char normfgcolor[]           = "#bbbbbb";
@@ -40,10 +40,14 @@ typedef struct {
 } Sp;
 const char *spcmd1[] = {"st", "-n", "spterm", "-g", "120x34", NULL };
 const char *spcmd2[] = {"discord", NULL};
+const char *spcmd3[] = {"st", "-n", "plainotes", "-e", "plainotes", "add", NULL};
+const char *spcmd4[] = {"st", "-n", "plainotes-browse", "-e", "plainotes", "browse", NULL};
 static Sp scratchpads[] = {
 	/* name          cmd  */
 	{"spterm",      spcmd1},
 	{"discord",     spcmd2},
+	{"plainotes",   spcmd3},
+	{"plainotes-browse", spcmd4},
 };
 
 /* tagging */
@@ -60,6 +64,8 @@ static const Rule rules[] = {
 	{ NULL,          NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
 	{ NULL,          "spterm", NULL,           SPTAG(0),  1,          1,           0,        -1 },
 	{ "discord",     NULL,     NULL,           SPTAG(1),  1,          0,           1,        -1 },
+	{ NULL,          "plainotes", NULL,        SPTAG(2),  1,          1,           0,        -1 },
+	{ NULL,          "plainotes-browse", NULL, SPTAG(3),  1,          1,           0,        -1 },
 };
 
 /* layout(s) */
@@ -69,7 +75,7 @@ static int resizehints = 1;    /* 1 means respect size hints in tiled resizals *
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ " ",      tile },    /* first entry is default */
+	{ " ",      tile },    /* first entry is default */
 	{ " ",      NULL },    /* no layout function means floating behavior */
 	{ " ",      monocle },
 };
@@ -126,6 +132,8 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_Return, togglescratch,  {.ui = 0 } },
 	{ MODKEY|ShiftMask,             XK_d,      togglescratch,  {.ui = 1 } },
+	{ MODKEY,                       XK_n,      togglescratch,  {.ui = 2 } },
+	{ MODKEY|ShiftMask,             XK_n,      togglescratch,  {.ui = 3 } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
