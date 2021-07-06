0d9bcde539c5cd0707a154a29208bac66d7fa163
Henrik Hansson <hinkik@gmail.com>
2021-07-06 16:01:25 +0200

    migrate keybindings to sxhkd

--- config.def.h
+++ config.def.h
@@ -87,7 +87,6 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
-#include <X11/XF86keysym.h>
 static const char *termcmd[]  = { "st", NULL };
 
 /*
@@ -106,8 +105,6 @@ ResourcePref resources[] = {
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_d,      spawn,          SHCMD("dmenu_run") },
-	{ MODKEY,                       XK_w,      spawn,          SHCMD("$BROWSER") },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
@@ -135,16 +132,6 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
 	{ MODKEY,                       XK_z,      incrgaps,       {.i = +1 } },
 	{ MODKEY,                       XK_x,      incrgaps,       {.i = -1 } },
-	/* Special key bindings */
-	{ MODKEY,                       XK_n,      spawn,          SHCMD("st -n plainotes -e plainotes") },
-	{ 0,                            XK_Print,  spawn,          SHCMD("maim -u | xclip -selection c -t image/png") },
-	{ ShiftMask,                    XK_Print,  spawn,          SHCMD("maim -su | xclip -selection c -t image/png") },
-	{ ControlMask,                  XK_Print,  spawn,          SHCMD("maim -i \"$(xdotool getactivewindow)\" | xclip -selection c -t image/png") },
-	{ 0, XF86XK_AudioMute,		spawn,     SHCMD("pamixer -t; kill -44 $(pidof dwmblocks)") },
-	{ 0, XF86XK_AudioRaiseVolume,	spawn,     SHCMD("pamixer --allow-boost -i 3; kill -44 $(pidof dwmblocks)") },
-	{ 0, XF86XK_AudioLowerVolume,	spawn,     SHCMD("pamixer --allow-boost -d 3; kill -44 $(pidof dwmblocks)") },
-	{ 0, XF86XK_MonBrightnessUp,	spawn,     SHCMD("xbacklight -inc 15") },
-	{ 0, XF86XK_MonBrightnessDown,	spawn,     SHCMD("xbacklight -dec 15") },
 	/*
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
